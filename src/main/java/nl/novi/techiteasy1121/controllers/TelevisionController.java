01d99728a88fda57082fddfe2eafb6f6964fc37c
MRensen <markrensen@hotmail.com>
2022-10-25 11:39:00 +0200

    deleted redundant method

--- src/main/java/nl/novi/techiteasy1121/controllers/TelevisionController.java
+++ src/main/java/nl/novi/techiteasy1121/controllers/TelevisionController.java
@@ -10,43 +10,37 @@ public class TelevisionController {
     @GetMapping("/televisions")
     public ResponseEntity<Object> getAllTelevisions() {
 
-        return ResponseEntity.ok();
+        return ResponseEntity.ok("televisions");
 
     }
 
     @GetMapping("/televisions/{id}")
     public ResponseEntity<Object> getTelevision() {
 
-        return ResponseEntity.ok();
+        return ResponseEntity.ok("television");
 
     }
 
     @PostMapping("/televisions")
     public ResponseEntity<Object> addTelevision(@RequestBody String television) {
 
-        return ResponseEntity.created();
+        return ResponseEntity.created(null).body("television");
 
     }
 
     @DeleteMapping("/televisions/{id}")
     public ResponseEntity<Object> deleteTelevision(@PathVariable int id) {
 
-        return ResponseEntity.noContent();
+        return ResponseEntity.noContent().build();
 
     }
 
     @PutMapping("/televisions/{id}")
     public ResponseEntity<Object> updateTelevision(@PathVariable int id, @RequestBody String television) {
 
-        return ResponseEntity.noContent();
+        return ResponseEntity.noContent().build();
 
     }
 
-    @GetMapping("/televisions?brand={brand}")
-    public ResponseEntity<Object> getAllTelevisions(@RequestParam String brand) {
-
-        return ResponseEntity.ok();
-
-    }
 
 }
