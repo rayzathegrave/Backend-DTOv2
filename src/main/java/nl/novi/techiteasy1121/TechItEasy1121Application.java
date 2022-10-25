e18a454249881d18034358fd1149dc01f55a1d50
MRensen <markrensen@hotmail.com>
2022-10-25 21:21:22 +0200

    added comment to main

--- src/main/java/nl/novi/techiteasy1121/TechItEasy1121Application.java
+++ src/main/java/nl/novi/techiteasy1121/TechItEasy1121Application.java
@@ -3,6 +3,11 @@ package nl.novi.techiteasy1121;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Deze hele klasse is standaard code die gegenereerd wordt bij het aanmaken van een nieuw Spring Boot project.
+// Deze klasse is, net als in "plain java", de voordeur van je applicatie. Hier starten we de applicatie.
+
+// Deze annotatie zorgt er voor dat Spring Boot alle configuraties klaar zet en eventuele custom configuraties inleest.
+// Ook de @RestController en @ControllerAdvice worden dankzij deze annotatie ingelezen.
 @SpringBootApplication
 public class TechItEasy1121Application {
 
